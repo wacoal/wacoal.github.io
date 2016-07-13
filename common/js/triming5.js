4123e0a833a11b94e21c26cab643c71fb2021a1e
odego <oodegoo@gmail.com>
2016-07-13 16:59:59 +0900

    var img = canvas; //拡大させる画像を定義
    var img = canvas; //拡大させる画像を定義

--- common/js/triming5.js
+++ common/js/triming5.js
@@ -85,30 +85,16 @@ function main(dataUrl) {
 				// maxHeight: elmMaxSize
 			});
 
-
-
 			//TODO function化したい
 			topOffset =  $("#js_triming_area").offset().top;
 			leftOffset = $("#js_triming_area").offset().left;
 			elmSize = scaleElmSize;
-			// $("#o-size").html(imageWidth);
-			// $('#now-size').html(elmSize);
-			// //$("#x").html(leftOffset);
-			// $("#x").html(maxScale);
-			// $("#y").html(topOffset);
-
 			elmX = ( ( repWidth/ 2) - (scaleElmSize / 2) ) + leftOffset;
 			elmY = ( (repHeight / 2) - (scaleElmSize / 2) );
-			// elmX = parseInt(elmX);
-			// elmY = parseInt(elmY);
-			// $("#x_now").html(elmX);
-			// $("#y_now").html(elmY);
 
 			ratio = figureScale(imageWidth,repWidth);
 
 			trimData();
-
-
 			test_info();
 
 		}, false);
@@ -151,24 +137,16 @@ function rotate(){
 		ctx.translate( -( angle == 180 || angle == 270 ? repWidth : 0), -( angle == 90|| angle == 180  ? repHeight : 0) );
 		ctx.drawImage(image, 0, 0, repWidth, repHeight);
 
-		$("#js_triming_area").css({
-			width: rotateWidth,
-			height: rotateHeight
-		});
-		$("#js_triming_element").css({
-			top: (rotateHeight / 2) - 120,
-			left: (rotateWidth /2) - 120,
-		});
-
-		elmMaxSize = repWidth < repHeight ? repWidth / 240 : repHeight / 240;
-
-		//TODO function1 回転したとき横は縦の値を入れるべきか
-		//  =  $(canvas).offset().top;
-		// leftOffset = $(canvas).offset().left;
-		// $("#o-size").html(imageWidth);
-		// $('#now-size').html(parseInt(repWidth));
-		// $("#x").html(leftOffset);
-		// $("#y").html();
+		// $("#js_triming_area").css({
+		// 	width: rotateWidth,
+		// 	height: rotateHeight
+		// });
+		// $("#js_triming_element").css({
+		// 	top: (rotateHeight / 2) - 120,
+		// 	left: (rotateWidth /2) - 120,
+		// });
+		//
+		// elmMaxSize = repWidth < repHeight ? repWidth / 240 : repHeight / 240;
 
 		trimRestore();
 }
@@ -183,6 +161,9 @@ function restore(){
 		ctx.save();
 }
 
+
+
+
 $(function(){
 
 	$(window).on("load",function(){
@@ -193,243 +174,96 @@ $(function(){
 		});
 	});
 
-	//ライブラリの初期設定
-	var $idTrimingArea = document.getElementById("js_triming_area");
-	//var $idTrimingArea = document.getElementById("canvas");
-	var $jqIdTrimingArea = $($idTrimingArea);
-	var $idTrimingElm = document.getElementById("js_triming_element");
-	var $jqIdTrimingElm = $($idTrimingElm);
-	var $hammerObj = new Hammer($idTrimingElm);
-	var $hammerObj2 = new Hammer($idTrimingArea);
-	var panTime = false;
-	var pinchTime = false;
-	var $pinchTimer = {};
-	$hammerObj.get("pan").set({ enable: true });
-	$hammerObj.get("pinch").set({ enable: true });
-	$jqIdTrimingElm.css("transform", "scale(1)");
+	var startDistance;
+	var moveDistance;
+	var currentScale = 1;
+	var saveScale = 1; //拡大率（縮小率）の初期値
+	var eventKind = '';
+	var elm = document.getElementById('animationLayer'); //タップを取得するエレメントを定義
+	//var img = document.getElementById('img01'); //拡大させる画像を定義
+	var img = canvas; //拡大させる画像を定義
 
-	// 小数点n位までを残す関数 (四捨五入)
-	function floatFormat( number, n ) {
-		var _pow = Math.pow( 10 , n ) ;
-		return Math.round( number * _pow ) / _pow ;
+	elm.addEventListener('touchstart', touchStart, false);//タップされた瞬間
+	elm.addEventListener('touchstart', touchMove, false);//指を動かしている
+	elm.addEventListener('touchstart', touchEnd, false);//指が画面から離れた
+
+	//タップされた瞬間に実行されるメソッド
+	function touchStart(e) {
+	    switch (e.touches.length) {
+	      case 1:
+	        eventKind = 'flick';
+	      case 2:
+	        eventKind = 'pinch';
+	        pinchStart(e);
+	        break;
+	      default:
+	        break;
+	    }
 	}
 
-	// var obj = $idTrimingElm;
-	// var rect = obj.getBoundingClientRect();
-	// //座標を計算する
-	// var positionX;
-	// var positionY;
-	// window.onload = function() {
-  //   var element = document.getElementById('canvas');
-  //   var rect = element.getBoundingClientRect();
-	//
-  //   console.log(rect.left);   // x座標(絶対座標)
-  //   console.log(rect.top);    // y座標(絶対座標)
-  //   console.log(rect.width);  // 幅
-  //   console.log(rect.height); // 高さ
-	//
-	// 	function getRect(){
-	// 		positionX = rect.left + window.pageXOffset;
-	// 		positionY = rect.top + window.pageYOffset;
-	// 	}
-	// }
+	//指が動いている時に実行されるメソッド
+	function touchMove(e) {
+	    e.preventDefault();
+	    switch (e.touches.length) {
+	      case 2:
+	        pinchMove(e);
+	        break;
+	      default:
+	        break;
+	    }
+	}
 
-	// var $idAreaImage = $("#js_triming_areaImage");
-	// $idAreaImage.load(function() {
-  //     //console.log('読み込み完了');
-	// 		//var element = document.getElementById('js_triming_areaImage');
-	// 		var originalWidth  = $(this).width();
-	// 		var originalHeight = $(this).height();
-	//     var originalTop    = $(this).offset().top;
-	// 		var originalLeft   = $(this).offset().left;
-	//
-	//     console.log(originalTop,originalLeft,originalWidth,originalHeight);   // x座標(絶対座標)
-	//     console.log(rect.top);    // y座標(絶対座標)
-	//     console.log(rect.width);  // 幅
-	//     console.log(rect.height); // 高さ
-  // });
-
-	$hammerObj.on("pan",function(event) {
-      if(event.isFinal) { //end
-          panTime = false;
-          $jqIdTrimingArea.data("down", false);
-          if(Number(String($jqIdTrimingElm.css("left")).replace("px", "")) < 0)
-              $jqIdTrimingElm.css("left", "0px");
-          if(Number(String($jqIdTrimingElm.css("left")).replace("px", "")) > ($jqIdTrimingArea.width() - $jqIdTrimingElm.width()))
-              $jqIdTrimingElm.css("left", ($jqIdTrimingArea.width() - $jqIdTrimingElm.width()) + "px");
-          if(Number(String($jqIdTrimingElm.css("top")).replace("px", "")) < 0)
-              $jqIdTrimingElm.css("top", "0px");
-          if(Number(String($jqIdTrimingElm.css("top")).replace("px", "")) > ($jqIdTrimingArea.outerHeight() - $jqIdTrimingElm.height()))
-              $jqIdTrimingElm.css("top", ($jqIdTrimingArea.outerHeight() - $jqIdTrimingElm.height()) + "px");
-					trimData();
-      } else {
-          if(!panTime) { //start
-              panTime = event.timeStamp;
-
-              $jqIdTrimingArea
-                  .data("down", true)
-                  .data("x", event.center.x)
-                  .data("y", event.center.y)
-                  .data("elmPosX", Number(String($jqIdTrimingElm.css("left")).replace("px", "")))
-                  .data("elmPosY", Number(String($jqIdTrimingElm.css("top")).replace("px", "")));
-          } else { //move
-              if ($jqIdTrimingArea.data("down") == true) {
-
-									elmX = ( ($jqIdTrimingArea.data("elmPosX") - ($jqIdTrimingArea.data("x") - event.center.x)) );
-									elmY = ( ($jqIdTrimingArea.data("elmPosY") - ($jqIdTrimingArea.data("y") - event.center.y)) );
-									// elmX = parseInt(elmX);
-									// elmY = parseInt(elmY);
-									if( elmX < 0){
-										elmX = 0;
-									}
-									if( elmX > repWidth - scaleElmSize ){
-										elmX = repWidth - scaleElmSize;
-									}
-									if( elmY < 0){
-										elmY = 0;
-									}
-									if( elmY > repHeight - scaleElmSize ){
-										elmY = repHeight - scaleElmSize;
-									}
-
-                  $jqIdTrimingElm.css({
-                      "left": elmX + "px",
-                      "top": elmY + "px"
-                  });
-
-									//$("#x_now").html(elmX);
-									//$("#y_now").html(elmY);
-              }
-          }
-      }
-  });
+	//指が離れた時に実行されるメソッド
+	function touchEnd(e) {
+	    switch (eventKind) {
+	      case 'pinch':
+	        pinchEnd(e);
+	        break;
+	      default:
+	        break;
+	    }
+	}
 
 
-	//pinch event
-	// var lock = false;
-	// if (!lock) {
-	// 	scaleElmSize = elmSize * scaleRatio;
-	// 	maxScale = elmSize / scaleElmSize;
-	// }
-	// lock = true;
-  $hammerObj2.on("pinch",function(event) {
+	function pinchStart(e){
 
-      event.preventDefault ? event.preventDefault() : (event.returnValue = false);
-      if(!pinchTime) { //start
-          pinchTime = event.timeStamp;
-          var preScale = String($jqIdTrimingElm.css("transform")).replace("matrix(", "");
-          preScale = preScale.replace(")", "");
-          preScale = preScale.split(",");
-          preScale = Math.sqrt(preScale[0] * preScale[0] + preScale[1] * preScale[1]);
-          $jqIdTrimingArea
-              .data("preScale", preScale)
-              .data("scale", event.scale);
+	  //スタート時の指の距離を保持
+	  startDistance = getDistance(e);
 
-      } else { //move
-          if($pinchTimer) clearTimeout($pinchTimer);
-					scaleSize = $jqIdTrimingArea.data("preScale") + (event.scale - $jqIdTrimingArea.data("scale"));
+	}
 
-					if( scaleSize > maxmax ){
-						scaleSize = maxmax
-					}
+	function pinchMove(e){
 
-					// if( scaleSize < 0.4 ){
-					// 	scaleSize = 0.4
-					// }
-					//scaleRatio = floatFormat( scaleSize, 3 );
+	  //2本指の距離を計算
+	  moveDistance = getDistance(e);
 
+	  //start時の距離を基準に動いた比率を計算
+	  currentScale = moveDistance / startDistance;
 
-					//scaleRatio = floatFormat( scaleSize, 1 );
-					scaleRatio = scaleSize;
+	  //拡大率を算出
+	  saveScale = saveScale * currentScale;
 
-					scaleElmSize = elmSize * scaleRatio;
+	  img.style.webkitTransform =
+	    'scale3d(' + saveScale + ',' + saveScale + ', 1)';
+	}
 
+	function pinchEnd(e){
+	  if(saveScale < 1){
+	    saveScale = 1;
+	    img.style.webkitTransform =
+	      'scale3d(' + saveScale + ',' + saveScale + ', 1)';
+	  }
+	}
 
-					// if( scaleRatio < 1 ){
-					// 	scaleElmSize = elmSize * scaleRatio;
-					// } else if( scaleRatio > 1 ){
-					// 	//alert(scaleRatio);
-					// } else {
-					//
-					// }
+	//指の距離を測るメソッド.
+	function getMeasure(e) {
+	 return Math.sqrt(
+	     Math.pow(
+	         Number(e.touches[0].pageX) - Number(e.touches[1].pageX), 2) +
+	     Math.pow(
+	         Number(e.touches[0].pageY) - Number(e.touches[1].pageY), 2));
+	}
 
-
-					// elmSize : scaleElmSize = 1 : x;
-					// scaleElmSize = elmSize * x;
-					// x= scaleElmSize / elmSize;
-
-
-
-					//
-					// scaleMaxWidth = repWidth / scaleElmSize;
-					// scaleMaxHeight = repHeight / scaleElmSize;
-					//maxScale = elmSize / scaleElmSize;
-					//
-					// elmX = elmX + ( scaleElmSize * scaleRatio );
-					// elmY = elmY + ( scaleElmSize * scaleRatio );
-
-					//scaleElmSize = scaleElmSize * scaleSize;
-					// scaleMaxWidth = repWidth / scaleElmSize;
-					// scaleMaxHeight = repHeight / scaleElmSize;
-					// maxScale = scaleMaxWidth < scaleMaxHeight ? scaleMaxWidth : scaleMaxHeight;
-
-          $jqIdTrimingElm.css({
-						"transform": "scale(" + scaleSize + ")"
-					});
-          $pinchTimer = setTimeout(function() { //end
-              pinchTime = false;
-          }, 100);
-
-					test_info();
-      }
-  });
-
-	$hammerObj2.on("pinchend",function(event) {
-
-		//var scaleSize = $jqIdTrimingArea.data("preScale") + (event.scale - $jqIdTrimingArea.data("scale"));
-
-
-		// if( scaleSize > maxScale ){
-		// 	scaleSize = maxScale
-		// }
-		// if( scaleSize < 0.4 ){
-		// 	scaleSize = 0.4
-		// }
-		// scaleRatio = floatFormat( scaleSize, 3 );
-		// scaleElmSize = elmSize * scaleRatio;
-		//
-		// scaleMaxWidth = repWidth / scaleElmSize;
-		// scaleMaxHeight = repHeight / scaleElmSize;
-		// maxScale = scaleMaxWidth < scaleMaxHeight ? scaleMaxWidth : scaleMaxHeight;
-
-		//alert(maxScale);
-		maxScale = elmSize / scaleElmSize;
-		maxmax   = scaleSize * limitScale;
-		if( maxmax > limitScale ){
-			maxmax = limitScale;
-		}
-		//limitScale = maxScale;
-
-		test_info();
-
-		//var maxElmWidth = scaleElmSize * maxScale;
-
-		elmX = ($jqIdTrimingElm.position().left);
-		elmY = ($jqIdTrimingElm.offset().top) - topOffset;
-
-		$jqIdTrimingElm.css({
-			width: scaleElmSize,
-			height: scaleElmSize,
-			"left": elmX,
-			"top": elmY,
-			"transform": "scale(none)",
-			//"border": 'solid 2px blue'
-		});
-
-
-		trimData();
-
-	});
 
 });
 
@@ -530,6 +364,10 @@ $('#js_btn_upload').off("click").on("click",function(e){
 	// 	preventEvent = true;
 	// }
 });
+
+
+
+
 
 var maxmax;
 var scaleSize;
