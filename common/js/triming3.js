b90ca743c879b16f686f4406b079876a70261c4a
odego <oodegoo@gmail.com>
2016-07-13 15:53:42 +0900

    ;
    ;

--- common/js/triming3.js
+++ common/js/triming3.js
@@ -328,7 +328,7 @@ $(function(){
       } else { //move
           if($pinchTimer) clearTimeout($pinchTimer);
 					var scaleSize = $jqIdTrimingArea.data("preScale") + (event.scale - $jqIdTrimingArea.data("scale"));
-					maxmax = scaleSize * limitScale
+					maxmax = scaleSize * limitScale;
 					if( scaleSize > maxmax ){
 						scaleSize = maxmax
 					}
