ee1f80593ce7f8be2c36b19c222a91ac48787cf2
odego <oodegoo@gmail.com>
2016-10-14 21:36:08 +0900

    add ajax
    add ajax

--- common/js/vue/vue_9_app.js
+++ common/js/vue/vue_9_app.js
@@ -32,3 +32,62 @@ var exsample = new Vue({
   el: '#example'
 });
 */
+
+
+
+testFunction: function(){
+  // var val = this.maxlength;
+  // console.log(val);
+  //e.preventDefault();
+  if (this.isValid) {
+    //document.getElementsByClassName("errors").className = "";
+    var args = {
+      url: '/ajax/member/{{ $member->id }}',
+      data: {
+        'pet': 'dfsa',
+      },
+      context:'ホゲホゲ',
+    };
+    ajax_post(args, function(data, textStatus, jqXHR) {
+      if (data && data.is_success) {
+        $(this).addClass('is_on');
+
+        // var count_ = $(this).find('.is_count');
+        // count_.text(parseInt(count_.text()) + 1);
+      } else {
+        console.log(textStatus);
+      }
+      //return false;
+
+    });
+
+
+    //
+    // $(".errors").addClass("errors_off");
+    // var jqxhr;
+    // if (jqxhr) {
+    //   return;
+    // }
+    // jqxhr = $.ajax({
+    // 	type: "POST",
+    //   url: '/ajax/member/{{ $member->id }}/',
+    //   //url: ajaxUrl,
+    // 	dataType: 'html',
+    //   timeout: 10000,
+    //   data: {
+    //     'pet': 'dfsa',
+    //   },
+    // })
+    // .done(function(data){
+    //   console.log("test");
+    // })
+    // .fail(function(data){
+    // 	console.log("fail");
+    // })
+    // .always(function(data){
+    //   return false;
+    // });
+  }else{
+    console.log("need edit")
+  }
+}
